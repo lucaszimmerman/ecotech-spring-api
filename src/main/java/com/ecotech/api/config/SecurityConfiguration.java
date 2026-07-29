7bec1c7246293ffe91a732961e4ecf4ed8334462
lucaszimmerman <lucaszimmermants@gmail.com>
2026-07-29 15:47:50 -0300

    feat(security): add initial security configuration

--- src/main/java/com/ecotech/api/config/SecurityConfiguration.java
+++ src/main/java/com/ecotech/api/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,20 +13,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                // APIs REST com JWT não utilizam autenticação baseada em sessão/cookies,
+                // portanto a proteção CSRF tradicional não é necessária.
+                .csrf(csrf -> csrf.disable())
+                // Define que a API é STATELESS.
+                // O servidor NÃO armazenará sessão do usuário.
+                // Cada requisição deverá enviar um JWT válido.
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users/**").permitAll()
-                        .anyRequest().authenticated());
-
-        return http.build();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 
 }
